30b725560e6e63ab1191020894eb85a7651df8b2
Ji Ho Nam <wlgh0123456@gmail.com>
2024-10-18 15:34:04 -0400

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,6 +9,7 @@ const Login = () => {
 
   return (
     <form
+      onSubmit={onSubmitHandler}
       className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
       action=""
     >
