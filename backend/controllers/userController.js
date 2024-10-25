4745a851b91b1f7367a2ef82d0b2cde076c54c73
Ji Ho Nam <wlgh0123456@gmail.com>
2024-10-25 13:56:16 -0400

    upload

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+// route for user login
+const loginUser = async (req, res) => {};
+
+// route for user register
+const registerUser = async (req, res) => {
+  res.json({ msg: "Register API Working" });
+};
+
+// route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
