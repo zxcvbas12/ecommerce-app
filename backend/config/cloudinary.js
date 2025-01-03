e8fa73f13961abeb30b472f6155dfee753743ad6
Ji Ho Nam <wlgh0123456@gmail.com>
2025-01-03 04:51:44 -0500

    update

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -4,7 +4,7 @@ const connectCloudinary = async () => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDNARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 };
 
