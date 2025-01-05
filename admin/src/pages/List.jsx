beb2674093d20ac0dc36378e84ffe64acbe14ab3
Ji Ho Nam <wlgh0123456@gmail.com>
2025-01-05 02:43:08 -0500

    update

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { backendUrl, currency } from "../App";
 import axios from "axios";
 import { toast } from "react-toastify";
+import PropTypes from "prop-types";
 
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
@@ -11,6 +12,26 @@ const List = () => {
       const response = await axios.get(backendUrl + "/api/product/list");
       if (response.data.success) {
         setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/product/remove",
+        { id },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
       } else {
         toast.error(response.data.message);
       }
@@ -40,20 +61,32 @@ const List = () => {
 
         {/* ------ Product List ------- */}
         {list.map((item, index) => (
-          <div key={index}>
-            <img src={item.image[0]} alt="" />
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
             <p>{item.name}</p>
             <p>{item.category}</p>
             <p>
               {currency}
               {item.price}
             </p>
-            <p>X</p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
           </div>
         ))}
       </div>
     </>
   );
+};
+
+List.propTypes = {
+  token: PropTypes.string.isRequired,
 };
 
 export default List;
