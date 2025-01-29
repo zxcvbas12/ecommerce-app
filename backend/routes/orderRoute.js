8994280c82771ab2ce1e67d205e7e48f228467b7
Ji Ho Nam <wlgh0123456@gmail.com>
2025-01-28 22:19:27 -0500

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,1 +1,22 @@
 import express from "express";
+import {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+// admin features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+// payment features
+orderRouter.post("/place", placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
